ce0cdb3113a79726cc0ac5c7d093fe237fb4869f
Tran Ngoc Thanh <thanhbk113@gmail.com>
2022-07-15 23:07:09 +0700

    Pointer

--- build-in-type-working/main.go
+++ build-in-type-working/main.go
@@ -2,35 +2,23 @@ package main
 
 import "fmt"
 
-// aggregate types (array, struct)
-
-type Car struct {
-	NumberOfTires int
-	Luxury        bool
-	BucketSeats   bool
-	Make          string
-	Model         string
-	Year          int
-}
+// aggregate types (pointer, struct)
 
 func main() {
 
-	myCar := Car{
-		NumberOfTires: 4,
-		Luxury:        true,
-		BucketSeats:   true,
-		Make:          "Ford",
-		Model:         "Fusion",
-		Year:          2015,
-	}
+	x := 10
 
-	// myCar.NumberOfTires = 4
-	// myCar.Luxury = false
-	// myCar.BucketSeats = true
-	// myCar.Make = "Ford"
-	// myCar.Model = "Fusion"
-	// myCar.Year = 2018
+	myFirstPointer := &x
 
-	fmt.Printf("My car is a %s %s %d\n", myCar.Make, myCar.Model, myCar.Year)
+	fmt.Println("x is:", x)
+	fmt.Println("myFirstPointer is:", myFirstPointer)
 
+	changeValueOfPointer(&x)
+
+	fmt.Println("x is after func called:", x)
+
+}
+
+func changeValueOfPointer(myPointer *int) {
+	*myPointer = 15
 }
