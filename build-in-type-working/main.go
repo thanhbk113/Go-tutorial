36ea8a9357a48ad5984caeb26a43cf33647eee96
Tran Ngoc Thanh <thanhbk113@gmail.com>
2022-07-16 16:49:36 +0700

    Expression

--- build-in-type-working/main.go
+++ build-in-type-working/main.go
@@ -2,55 +2,23 @@ package main
 
 import "fmt"
 
-//interface{} is a blank interface
-
-type Animal interface {
-	Says() string
-	HowManyLegs() int
-}
-
-type Dog struct {
-	Name         string
-	Sound        string
-	NumberOfLegs int
-}
-
-func (d *Dog) Says() string {
-	return d.Sound
-}
-
-func (d *Dog) HowManyLegs() int {
-	return d.NumberOfLegs
-}
-
-func (d *Cat) Says() string {
-	return d.Sound
-}
-
-func (d *Cat) HowManyLegs() int {
-	return d.NumberOfLegs
-}
-
-type Cat struct {
-	Name         string
-	Sound        string
-	NumberOfLegs int
-	HasTail      bool
-}
+//expression
 
 func main() {
 
-	d := Dog{Name: "dog", Sound: "Woof", NumberOfLegs: 4}
-	Riddle(&d)
-	var cat Cat
-	cat.Name = "cat"
-	cat.Sound = "Meow"
-	cat.NumberOfLegs = 4
-	cat.HasTail = true
-	Riddle(&cat)
-}
+	age := 10
+	name := "Jack"
+	rightHanded := true
 
-func Riddle(a Animal) {
-	riddle := fmt.Sprintf(`This is animal says "%s" and has %d legs.What animal is it?`, a.Says(), a.HowManyLegs())
-	fmt.Println(riddle)
+	fmt.Printf("%s is %d years old.Right handed : %t\n", name, age, rightHanded)
+
+	fmt.Println()
+
+	ageInTenYears := age + 10 // expression
+
+	fmt.Printf("%s will be %d years old in 10 years.\n", name, ageInTenYears)
+
+	isATeenager := ageInTenYears >= 13 // expression
+
+	fmt.Printf("%s is a teenager : %t\n", name, isATeenager)
 }
