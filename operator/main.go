acf1189065950c06d65724396325935159c456b2
Tran Ngoc Thanh <thanhbk113@gmail.com>
2022-07-17 16:42:45 +0700

    operator

---
+++ operator/main.go
@@ -0,0 +1,15 @@
+package main
+
+func main() {
+
+	//does one number divide exactly into another?
+	// x := 12
+	// y := 5
+
+	// if x%y == 0 {
+	// 	println(x, "divides evenly into", y)
+	// } else {
+	// 	println(x, "does not divide evenly into", y)
+	// }
+
+}
