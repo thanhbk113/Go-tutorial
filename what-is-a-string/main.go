e08135770e7462d3e17fad9da635d693b26585b0
Tran Ngoc Thanh <thanhbk113@gmail.com>
2022-07-17 20:05:58 +0700

    func package strings

--- what-is-a-string/main.go
+++ what-is-a-string/main.go
@@ -1,54 +1,23 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
+import "strings"
 
 func main() {
+	courses := []string{"Learn Go to Beginner", "Learn Go to Intermediate", "Learn Go to Advanced"}
 
-	fmt.Println()
-	name := "Hello World"
+	for _, course := range courses {
 
-	fmt.Println(name)
-
-	fmt.Println("Bytes")
-
-	for i := 0; i < len(name); i++ {
-		fmt.Printf("%x ", name[i])
+		if strings.Contains(course, "Go") {
+			println("Go is found in", course, "and index is", strings.Index(course, "Go"))
+		}
 	}
 
-	fmt.Println()
-	fmt.Println()
+	newString := "Go is a great programming language.Go for it!"
 
-	fmt.Println("Index\tRunes\tString")
-
-	for i, c := range name {
-		fmt.Println(i, "\t", c, "\t", string(c))
-	}
-
-	fmt.Println()
-
-	h := "Hello"
-	w := "World"
-
-	myString := fmt.Sprintf("%s %s", h, w)
-	fmt.Println("myString:", myString)
-	fmt.Println()
-
-	//using string builder
-
-	sb := new(strings.Builder)
-	sb.WriteString(h)
-	sb.WriteString(" ")
-	sb.WriteString(w)
-	fmt.Println("sb:", sb.String())
-
-	fmt.Println()
-
-	name = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
-	fmt.Println("Getting a substring")
-	fmt.Println(name[2:5])
-
+	println(strings.HasPrefix(newString, "Go"))
+	println(strings.HasSuffix(newString, "Python"))
+	println(strings.HasSuffix(newString, "!"))
+	println(strings.Count(newString, "Go"))
+	println(strings.Count(newString, "Python"))
+	println(strings.Index(newString, "Python"))
 }
